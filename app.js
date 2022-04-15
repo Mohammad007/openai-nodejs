0c974acb90a4c90cc774cf68ab15c5e2b5fa6b1a
James <jmskarte@gmail.com>
2022-04-16 00:10:42 +0530

    make it better

--- app.js
+++ app.js
@@ -1,33 +1,37 @@
 const express = require("express");
 const { Configuration, OpenAIApi } = require("openai");
-const app = express()
-const port = 5000
+const app = express();
+const cors = require("cors");
+const port = 5000;
 
-app.use(express.json())
+app.use(express.json());
+app.use(cors())
 
-app.post('/' , async (req , res)=>{
-    const { message } = req.body
-    if(!message) return res.status(400).json({message: "Please provide a message"})
-    try {
-        const configuration = new Configuration({
-            apiKey: "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",
-          });
-          const openai = new OpenAIApi(configuration);
-          
-          const response = await openai.createCompletion("text-davinci-002", {
-            prompt: `${message}`,
-            temperature: 0,
-            max_tokens: 60,
-            top_p: 1.0,
-            frequency_penalty: 0.0,
-            presence_penalty: 0.0,
-          });
-          res.status(200).send(response.data.choices[0].text)
-    } catch (error) {
-        console.log("Error ",error);
-        res.status(500).json({message: "Internal Server Error"})
-    }
-})
+app.post("/", async (req, res) => {
+  const { message } = req.body;
+  if (!message)
+    return res.status(400).json({ message: "Please provide a message" });
+  try {
+    const configuration = new Configuration({
+      apiKey: "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",
+    });
+    const openai = new OpenAIApi(configuration);
 
+    const response = await openai.createCompletion("text-davinci-002", {
+      prompt: `Correct this to standard English: ${message}`,
+      temperature: 0,
+      max_tokens: 60,
+      top_p: 1.0,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0,
+    });
+    res.status(200).send(response.data.choices[0].text);
+  } catch (error) {
+    console.log("Error ", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
 
-app.listen(port , ()=> console.log('> Server is up and running on port : ' + port))
+app.listen(port, () =>
+  console.log("> Server is up and running on port : " + port)
+);
